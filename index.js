7714b14b99e01f943875d34c115f72e5c0748b91
eggcm <52985883+eggcm@users.noreply.github.com>
2025-06-26 13:10:34 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ function display() {
     Final Demo  (20%): ${data[id].Final}
     Final Report(25%): ${data[id].Report}
 
-    Attendance (including taking surveys)(5%)
-    Attend: ${data[id].Attend}
+    Attendance (classes, surveys, etc. for 4% and 1% for April 02, 2025)
+    Attend: ${data[id].Attend} 
     
     Semester Grade (Mark): ${data[id].SEM}
     `;
